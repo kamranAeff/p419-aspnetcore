df899e14fb946b1ea47435eb89b13545d7cb5db2
kamranAeff <kamran_aeff@mail.ru>
2024-06-29 15:00:01 +0400

    validation filter done

---
+++ src/Presentation/WebUI/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebUI.Filters
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            context.ExceptionHandled = true;
+            context.Result = new ContentResult()
+            {
+                Content = "biraz sonra yeniden yoxlayin",
+                ContentType = "text/plain",
+                StatusCode = 200
+            };
+            Console.WriteLine($"TesExceptionFilter {DateTime.Now:HH:mm:ss.ffffff}\n{context.Exception.Message}");
+        }
+    }
+}
