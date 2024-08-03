ca89e8b4143b1c5390d2fb41cc94277551a1e65f
kamranAeff <kamran_aeff@mail.ru>
2024-08-03 11:28:59 +0400

    signal-r done

---
+++ src/Core/Domain/StableModels/Degrees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.StableModels
+{
+    public enum Degrees
+    {
+        Bachelor,
+        Master,
+        Doctorate
+    }
+}
