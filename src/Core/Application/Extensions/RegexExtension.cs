096c0f1d55a1a7950aafaaeadc80c92a958d61c2
kamranAeff <kamran_aeff@mail.ru>
2024-08-31 15:01:02 +0400

    extension optimization

--- src/Core/Application/Extensions/RegexExtension.cs
+++ src/Core/Application/Extensions/RegexExtension.cs
@@ -44,14 +44,8 @@ namespace Application.Extensions
             return value;
         }
 
-        static public bool IsMail(this string value)
-        {
-            return Regex.IsMatch(value, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", RegexOptions.IgnoreCase);
-        }
+        static public bool IsMail(this string value) => Regex.IsMatch(value, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", RegexOptions.IgnoreCase);
 
-        static public bool IsPhone(this string value)
-        {
-            return Regex.IsMatch(value, @"^(\+994|0)(50|51|55|70|77|99|10)-(\d{3}-\d{2}-\d{2})$");
-        }
+        static public bool IsPhone(this string value) => Regex.IsMatch(value, @"^(\+994|0)(50|51|55|70|77|99|10)-(\d{3}-\d{2}-\d{2})$");
     }
 }
